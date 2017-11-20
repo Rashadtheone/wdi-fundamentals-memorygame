911a2d54111e04170711ee0269b986e1e769dfb6
Rashad Hammonds <rhammonds@yearup.org>
2017-11-20 15:18:47 -0500

    scripting Updating

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,52 @@
-//console.log("Swag");
-var cards = ["queen","queen","king","king"];
-var cardsInPlay = [];
+// console.log("Swag");
+var cards = [
+  {
+    rank: 'queen',
+    suite: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
 
-var checkForMatch = function() {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!")
-        } else if(cardsInPlay.length !== 2){
-        console.log("You need another card bro.")
-        } else {
-        alert("Sorry, try again.")
-        }
+  {
+    rank: 'queen',
+    suite: 'Diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
 
+  {
+    rank: 'king',
+    suite: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+
+  {
+    rank: 'king',
+    suite: 'Diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+
+]
+var cardsInPlay = []
+
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!')
+  } else if (cardsInPlay.length !== 2) {
+    console.log('You need another card bro.')
+  } else {
+    alert('Sorry, try again.')
+  }
 }
 
-var flipcard = function(cardId) { 
+var flipcard = function (cardId) {
+  console.log('User flipped' + cards[cardId].rank)
 
-    console.log("User flipped" + cards[cardId])
+  cardsInPlay.push(cards[cardId])
 
-    cardsInPlay.push(cards[cardId])
+  checkForMatch()
 
-    checkForMatch()
-    
+  console.log(cards[cardId].cardImage)
+  console.log(cards[cardId].suite)
 }
-flipcard(2);
-flipcard(1);
+
+flipcard(2)
+flipcard(1)
