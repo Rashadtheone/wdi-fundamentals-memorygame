4637e1405dbb9bcb03cce99276e93c2e3b6538ab
Rashad Hammonds <rhammonds@yearup.org>
2017-11-19 22:32:48 -0500

    another update with script

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+//console.log("Swag");
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+        } else {
+        alert("Sorry, try again.");
+        }
+}
+
+var flipcard = function(cardId) { 
+
+    console.log("User flipped" + cards[cardId])
+
+    cardsInPlay.push(cards[cardId])
+
+    checkForMatch()
+    
+}
+flipcard(2);
+flipcard(1);
